a71bc39e7c47d443c22c3a4251ccf22ab4b3da1b
sjaustirni <22116610+sjaustirni@users.noreply.github.com>
2023-09-21 16:18:19 +0200

    reformat file format.py

--- filter.py
+++ filter.py
@@ -18,11 +18,11 @@ class Filter:
         self.bandpass_high = bandpass_high
         self.bandpass_order = bandpass_order
         self.mean_kernel_size = mean_kernel_size if mean_kernel_size else int(self.fs / 3)
-
+        
         self.input = []
         self.bandpassed = []
         self.output = []
-
+        
         self.b, self.a = self._create_bandpass()
         self.zi = lfilter_zi(self.b, self.a)
         
@@ -34,7 +34,7 @@ class Filter:
     def _create_bandpass(self):
         b, a = iirfilter(self.bandpass_order, [self.bandpass_low, self.bandpass_high], fs = self.fs, btype='band', ftype='butter')
         return b, a
-
+    
     def apply(self, sample):
         """
         :param sample: New sample to be filtered
@@ -42,10 +42,10 @@ class Filter:
         """
         # Rectify the input signal
         sample = abs(sample)
-
+        
         # Add the sample to the input buffer
         self.input.append(sample)
-
+        
         # Apply the bandpass filter
         sample, self.zi = lfilter(self.b, self.a, [sample], zi=self.zi)
         sample = abs(sample[0])
@@ -58,25 +58,24 @@ class Filter:
             sample = np.mean(self.bandpassed + [sample])
 
         self.output.append(sample)
-
+        
         return sample
-
+    
     def mark_as_baseline(self):
         if self.baseline_start is None:
             self.baseline_start = len(self.output)
-
+    
     def mark_as_NOT_baseline(self):
         if self.baseline_start is None:
             return
         if self.baseline_end is None:
             self.baseline_end = len(self.output)
-            actual_end = int(250/2)  # half a second
-            actual_start = 250 * 1 + actual_end # 1 second
-            data = self.output[self.baseline_end-actual_start:self.baseline_end-actual_end]
+            actual_end = int(250 / 2)  # half a second
+            actual_start = 250 * 1 + actual_end  # 1 second
+            data = self.output[self.baseline_end - actual_start:self.baseline_end - actual_end]
             self.baseline = np.mean(data)
             self.baseline_std = np.std(data)
-
-
+    
     def reset_baseline(self):
         self.baseline_start = None
         self.baseline_end = None
